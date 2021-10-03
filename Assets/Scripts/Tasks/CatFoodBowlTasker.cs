68819701159dbb1891d80d2ba7e096f4e8a0a9af
christopherhurt <chris526@vt.edu>
2021-10-03 13:32:23 -0400

    All tasks with art done

--- Assets/Scripts/Tasks/CatFoodBowlTasker.cs
+++ Assets/Scripts/Tasks/CatFoodBowlTasker.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class CatFoodBowlTasker : Tasker
 {
     
+    public SpriteRenderer bowlImage;
+    public Sprite[] bowlSprites;
     public int expectedFood;
     
     private int foodCount;
     
     void Start() {
-        foodCount = 0;
+        Reset();
     }
     
     public void AddFood() {
         foodCount++;
-        // TODO: update image
+        UpdateFoodSprite();
         if (foodCount >= expectedFood) {
             director.UpdateProgress(1.0f);
         }
     }
     
+    private void UpdateFoodSprite() {
+        if (bowlSprites.Length > 0) {
+            float lerp = Mathf.Max(0.0f, Mathf.Min(0.999f, (float)foodCount / expectedFood));
+            int spriteIndex = (int)(lerp * bowlSprites.Length);
+            bowlImage.sprite = bowlSprites[spriteIndex];
+        }
+    }
+    
     public override void Reset() {
         foodCount = 0;
-        // TODO: reset image
+        UpdateFoodSprite();
     }
     
 }
