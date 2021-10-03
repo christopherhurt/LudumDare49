969f889f9fd9eeccba2e7cd8a69c80917d006a50
christopherhurt <chris526@vt.edu>
2021-10-03 13:12:18 -0400

    Generator and computer tasks done

--- Assets/Scripts/Tasks/ComputerTarget.cs
+++ Assets/Scripts/Tasks/ComputerTarget.cs
@@ -8,9 +8,11 @@ public class ComputerTarget : MonoBehaviour
     public ComputerTasker tasker;
     public TaskDirector director;
     public TaskManager manager;
+    public float opacityDegreeOdd;
     
     void Update() {
         float remaining = manager.GetTimeRemaining(TaskManager.TaskType.COMPUTER) / director.GetTimeout();
+        remaining = Mathf.Pow(remaining - 1.0f, opacityDegreeOdd) + 1.0f;
         SpriteRenderer renderer = GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
         renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, remaining);
     }
